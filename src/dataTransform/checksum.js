63254b511fbb686aa56ca7fc5c2a22cfc1c17c56
Yashvardhan Mathur <43027504+yvmatts@users.noreply.github.com>
2021-12-27 18:17:12 +0530

    Crypto js added. Non essential encryption code removed

--- src/dataTransform/checksum.js
+++ src/dataTransform/checksum.js
@@ -1,13 +1,13 @@
 const rand = require('./randomize')
 const hash = require('object-hash')
-
+const SHA256 = require("crypto-js/sha256")
 function checksum() {
   const getDs = rand.randomize()
   let checksum_ds = getDs()
 
   return function getCheckSumDs() {
     for(let i = 0; i< checksum_ds.length; i++) {
-      checksum_ds[i].secretKey = hash('' + checksum_ds[i].name + checksum_ds[i].origin + checksum_ds[i].destination, {algorithm:'sha256'})
+      checksum_ds[i].secretKey = SHA256('' + checksum_ds[i].name + checksum_ds[i].origin + checksum_ds[i].destination).toString()
     }
     return checksum_ds
   }
--- src/dataTransform/encrypt.js
+++ src/dataTransform/encrypt.js
@@ -1,15 +1,15 @@
-const aes256 = require('aes256')
-const conf = require('../conf')
 const cs = require('./checksum')
+const CryptoJS = require("crypto-js")
 
 function encrypt() {
   const getCheckSumDs = cs.checksum()
   let encrypted_ds = getCheckSumDs()
   return function getEncryptedDs() {
     for(let i = 0; i<encrypted_ds.length; i++) {
-      encrypted_ds[i] = aes256.encrypt(conf.secretKey, '' + encrypted_ds[i])
+      encrypted_ds[i] = CryptoJS.AES.encrypt('' + encrypted_ds[i],conf.secretKey).toString()
     }
     return encrypted_ds
   }
 }
+
 module.exports = {encrypt}
